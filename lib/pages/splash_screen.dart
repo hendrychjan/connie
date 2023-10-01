8db1b2bb5501ac0d0a2b2928f2b479ab4d43c78b
hendrychjan <jan.honza.hendrych@gmail.com>
2023-10-01 22:08:09 +0200

    Setup Android apk builds

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Future.delayed(const Duration(seconds: 5), () async {
+    Future.delayed(Duration.zero, () async {
       await InitService.initApp();
 
       if (AppController.to.firstTimeOpened) {
